73741f011409c4bc5d041de9fb795cae1eaa0ff0
andreapaccagnella94 <andreapacca94@live.it>
2025-07-16 20:07:05 +0200

    add the second exercise has begun

--- main.js
+++ main.js
@@ -4,13 +4,6 @@ Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
-Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.
-
 Consigli del giorno
 Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
@@ -60,6 +53,57 @@ if (wordTest === reversedWord(wordTest)) {
 
 
 // altro esercizio
+
+/*
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+Consigli del giorno
+Scriviamo sempre in italiano i passaggi che vogliamo fare
+Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+Domande da  farsi quando si crea una funzione:
+Come dovrebbe chiamarsi?
+Ho bisogno di parametri?
+Devo restituire un valore?
+Se sì, di che tipo?
+
+*/
+// faccio scegliere all'utente se vuole pari o dispari
+const evenOddUser = (prompt("Scegli tra pari e dispari e scrivilo qui"));
+console.log(evenOddUser);
+
+// faccio scegliere all'utente che numero vuole tra 1 e 5
+const numberUser = Number(prompt("Scegli un numero tra 1 e 5 e scrivilo qui"));
+console.log(numberUser);
+
+
+// creo le variabili per indicare tra che numeri creo il numero random 
+const minRandom = 1;
+const maxRandom = 5;
+
+// funzione copiata da w3school per generare un numero a caso tra due numeri compresi
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//genero un numero a caso con la mia funzione e lo salvo in una costante
+
+let numberPc = getRndInteger(minRandom, maxRandom);
+console.log(numberPc);
+
+//somma semplice tra i due numeri
+
+let sumEasy = numberUser + numberPc;
+console.log(sumEasy);
+
+//creo una funzione per verificare se la somma è pari o dispari
 
 
 
